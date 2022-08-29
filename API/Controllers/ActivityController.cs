ea88d79553b6a53c3df0182be1ae9c1129282e91
Bec Johnson <bec@alienoutfits.com>
2022-08-29 17:03:11 -0400

    Created Activity Controller

---
+++ API/Controllers/ActivityController.cs
@@ -0,0 +1,32 @@
+
+
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActivityController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivityController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
