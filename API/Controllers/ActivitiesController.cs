39fac4f78c819b19e900d6eeabb5814abde4863d
Bec Johnson <bec@alienoutfits.com>
2022-08-31 16:27:15 -0400

    Added CRUD Controllers on API

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,45 @@
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActivitiesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new List.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> EditActivity(Guid id, Activity activity) 
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(Guid id) 
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+        }
+    }
+}
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,32 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+import { List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
   
   useEffect(() => {
-    axios.get('https://localhost:5000/api/activities').then(response => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
       console.log(response);
       setActivities(response.data);
     })
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+        <List>
           {activities.map((activity:any) => (
-          <li key={activity.id}>
+          <List.Item key={activity.id}>
             {activity.title}
-          </li>
+          </List.Item>
           ))}
-        </ul>
-      </header>
+        </List>
     </div>
   );
 }
 
 export default App;
+
+//<img src={logo} className="App-logo" alt="logo" />
