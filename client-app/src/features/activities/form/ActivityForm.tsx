a7aa25feeb87168148de39ab19aea08189c4a3fb
Bec Johnson <bec@alienoutfits.com>
2022-09-01 13:58:04 -0400

    GUID for activity id

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -37,16 +37,21 @@ export default function ActivityForm({activity : selectedActivity, closeForm, cr
                 <Form.Input placeholder='Title' value={activity.title} name='title' 
                 onChange={handleInputChange} 
                 />
-                <Form.TextArea placeholder='Description' value={activity.title} name='title' 
-                onChange={handleInputChange} />
-                <Form.Input placeholder='Category' value={activity.category} name='title' 
-                onChange={handleInputChange} />
-                <Form.Input placeholder='Date' value={activity.date} name='title' 
-                onChange={handleInputChange} />
-                <Form.Input placeholder='City' value={activity.city} name='title' 
-                onChange={handleInputChange} />
-                <Form.Input placeholder='Venue' value={activity.venue} name='title' 
-                onChange={handleInputChange} />
+                <Form.TextArea placeholder='Description' value={activity.title} name='description'  
+                onChange={handleInputChange} 
+                />
+                <Form.Input placeholder='Category' value={activity.category} name='category'  
+                onChange={handleInputChange} 
+                />
+                <Form.Input placeholder='Date' value={activity.date} name='date' 
+                onChange={handleInputChange} 
+                />
+                <Form.Input placeholder='City' value={activity.city} name='city' 
+                onChange={handleInputChange} 
+                />
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' 
+                onChange={handleInputChange} 
+                />
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
